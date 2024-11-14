20e7b909c21e918314134af53fe614b0e15ee7ad
tinasche <tinashec93@live.com>
2024-11-14 16:55:31 +0200

    chore: setup for h2 persistence with jdbc

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    runtimeOnly("com.h2database:h2")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
